524f6280d5e460b01e264216d82480c570e6cecf
Abicoc4j@ <Anto.a@smartan.ai>
2025-12-11 13:27:31 +0530

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/app/_components/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
